930220c2bf2917872286208ab40332a0d5606c3b
Ajay Halthor <Ajay@Ajays-MacBook-Pro.local>
2017-04-20 23:26:05 +0530

    Get candidate keywords

--- keywords.py
+++ keywords.py
@@ -1,10 +1,10 @@
 """
-Takes document as input and performs chunking accoring to a pattern to return aa list of Candidate Keywords.
+Takes document as input and performs chunking accoring to a pattern to return a list of Candidate Keywords.
 
 Input :
-	document - text file
+    document - text file
 Output :
-	list of possible keyphrases
+    list of possible keyphrases
 
 """
 import nltk
@@ -14,23 +14,23 @@ from nltk.chunk import RegexpParser
 
 def extract_candidate_keywords(document):
 
-	#Get the words in the document
-	words = word_tokenize(document)
+    #Get the words in the document
+    words = word_tokenize(document)
 
-	# Chunk first to get 'Candidate Keywords'
-	tagged = nltk.pos_tag(words)
-	chunkGram = r""" PHRASE: 
-						{(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}
-				"""
+    # Chunk first to get 'Candidate Keywords'
+    tagged = nltk.pos_tag(words)
+    chunkGram = r""" PHRASE: 
+                        {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}
+                """
 
-	chunkParser = RegexpParser(chunkGram)
-	chunked = chunkParser.parse(tagged)
+    chunkParser = RegexpParser(chunkGram)
+    chunked = chunkParser.parse(tagged)
 
-	candidate_keywords = []
-	for tree in chunked.subtrees():
-		if tree.label() == 'PHRASE':
-			candidate_keyword = ' '.join([x for x,y in tree.leaves()])
-			candidate_keywords.append(candidate_keyword)
+    candidate_keywords = []
+    for tree in chunked.subtrees():
+        if tree.label() == 'PHRASE':
+            candidate_keyword = ' '.join([x for x,y in tree.leaves()])
+            candidate_keywords.append(candidate_keyword)
 
-	candidate_keywords = [w for w in candidate_keywords if len(w) > 3 and  len(w.split(' ')) < 6] 
-	return candidate_keywords
+    candidate_keywords = [w for w in candidate_keywords if len(w) > 3 and  len(w.split(' ')) < 6] 
+    return candidate_keywords
